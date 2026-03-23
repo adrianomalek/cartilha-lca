1b67286554855bb0268ff399f747e4baf575b6f5
adrianomalek <adrianomalek01@gmail.com>
2026-03-23 19:00:58 -0300

    Implement service worker caching for assets

--- sw.js
+++ sw.js
@@ -1,1 +1,21 @@
+const CACHE_NAME = "cartilha-cache-v1";
 
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => {
+      return cache.addAll([
+        "./",
+        "index.html",
+        "logo.jpg"
+      ]);
+    })
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
